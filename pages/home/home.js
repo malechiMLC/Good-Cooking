b292ab8fbf78a1db1e2039aec2144611ca63f0ea
JasonZhu-WHU <15927601190@163.com>
2020-03-11 22:16:45 +0800

    0311-zhujie init home page

---
+++ pages/home/home.js
@@ -0,0 +1,122 @@
+// pages/home/home.js
+const order = ['demo1', 'demo2', 'demo3']
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    // 是否显示面板指示点
+    indicatorDots: true,
+    // 滑动方向是否为纵向
+    vertical: false,
+    // 是否自动切换
+    autoplay: true,
+    // 是否采用衔接滑动
+    circular: true,
+    // 自动切换时间间隔
+    interval: 2000,
+    // 滑动动画时长
+    duration: 500,
+    // 前边距，可用于露出前一项的一小部分
+    previousMargin: 10,
+    // 后边距，可用于露出后一项的一小部分
+    nextMargin: 10,
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  scroll(e) {
+    console.log(e)
+  },
+
+  scrollToTop() {
+    this.setAction({
+      scrollTop: 0
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  changeProperty: function (e) {
+    var propertyName = e.currentTarget.dataset.propertyName
+    var newData = {}
+    newData[propertyName] = e.detail.value
+    this.setData(newData)
+  },
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
